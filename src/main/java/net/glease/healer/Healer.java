066c06af898315bc0a20cc4b4bb5afe71b7fd230
Glease <4586901+Glease@users.noreply.github.com>
2021-12-10 06:28:45 +0800

    Make it work for minecraft revisions up to 1.12
    Why no later minecraft revisions? Ask forge people not me. If they can hold back their hostility towards legacy minecraft mod developers I might consider adding a version for them, or they can copy over stuff and make it their own stuff.

--- src/main/java/net/glease/healer/Healer.java
+++ src/main/java/net/glease/healer/Healer.java
@@ -6,33 +6,52 @@ import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.impl.Log4jContextFactory;
 import org.apache.logging.log4j.core.lookup.Interpolator;
 import org.apache.logging.log4j.core.lookup.StrLookup;
+import org.apache.logging.log4j.spi.LoggerContextFactory;
 
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
+import java.util.WeakHashMap;
 
 import static net.glease.healer.Healer.MOD_ID;
 import static net.glease.healer.Healer.MOD_PACKAGE;
 
 @cpw.mods.fml.relauncher.IFMLLoadingPlugin.Name(MOD_ID)
 @cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions(MOD_PACKAGE)
-// log4j 2.11.x+ renamed lookups to strLookupMap
-// IDK since which minecraft version it breaks, so let's stay safe
-@cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion("1.7.10")
-public class Healer implements cpw.mods.fml.relauncher.IFMLLoadingPlugin {
+@net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.Name(MOD_ID)
+@net.minecraftforge.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions(MOD_ID)
+public class Healer implements cpw.mods.fml.relauncher.IFMLLoadingPlugin, net.minecraftforge.fml.relauncher.IFMLLoadingPlugin {
     public static final String MOD_ID = "healer";
     public static final String MOD_PACKAGE = "net.glease.healer";
     static final Field fieldLookup;
 
     static {
+        Field fieldLookup1;
+        Class<?> clazz;
         try {
-            Class<?> clazz = Class.forName("org.apache.logging.log4j.core.lookup.Interpolator");
-            fieldLookup = clazz.getDeclaredField("lookups");
-            fieldLookup.setAccessible(true);
+            clazz = Class.forName("org.apache.logging.log4j.core.lookup.Interpolator");
         } catch (ReflectiveOperationException e) {
             throw panic(e);
         }
+        try {
+            fieldLookup1 = clazz.getDeclaredField("lookups");
+            fieldLookup1.setAccessible(true);
+        } catch (ReflectiveOperationException e) {
+            try {
+                //noinspection JavaReflectionMemberAccess
+                fieldLookup1 = clazz.getDeclaredField("strLookupMap");
+                fieldLookup1.setAccessible(true);
+            } catch (ReflectiveOperationException ex) {
+                throw panic(ex);
+            }
+        }
+        fieldLookup = fieldLookup1;
     }
 
     static PrintStream log;
@@ -82,7 +101,8 @@ public class Healer implements cpw.mods.fml.relauncher.IFMLLoadingPlugin {
             }
             Field factory = LogManager.class.getDeclaredField("factory");
             factory.setAccessible(true);
-            factory.set(null, new WrappingLogContextFactory(LogManager.getFactory()));
+            factory.set(null, Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[]{LoggerContextFactory.class}, new LoggerContextFactoryProxy(LogManager.getFactory())));
+            log("Replaced factory");
         } catch (ReflectiveOperationException e) {
             throw panic(e);
         }
@@ -106,5 +126,34 @@ public class Healer implements cpw.mods.fml.relauncher.IFMLLoadingPlugin {
     @Override
     public String getAccessTransformerClass() {
         return null;
+    }
+
+    private static class LoggerContextFactoryProxy implements InvocationHandler {
+        private final LoggerContextFactory backing;
+        private final Set<org.apache.logging.log4j.spi.LoggerContext> known = Collections.synchronizedSet(Collections.newSetFromMap(new WeakHashMap<>()));
+
+        public LoggerContextFactoryProxy(LoggerContextFactory backing) {
+            this.backing = backing;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if ("getContext".equals(method.getName())) {
+                Object result = method.invoke(backing, args);
+                if (result instanceof LoggerContext) {
+                    LoggerContext casted = (LoggerContext) result;
+                    if (known.add(casted)) {
+                        log("Accepting new context post launch");
+                        processCoreContext(casted);
+                    }
+                }
+                return result;
+            } else if ("removeContext".equals(method.getName())) {
+                if (args[0] instanceof LoggerContext) {
+                    known.remove(args[0]);
+                }
+            }
+            return method.invoke(backing, args);
+        }
     }
 }
